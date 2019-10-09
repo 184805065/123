afc8867ddc37a64124f41279aa147f215f3821ea
184805065 <55075106+184805065@users.noreply.github.com>
2019-10-09 23:29:30 +0800

    Add files via upload

---
+++ abcd/src/MainClass.java
@@ -0,0 +1,11 @@
+package mainclass;
+import course.*;
+import student.*;
+import teacher.*;
+public class MainClass{
+           public static void main(String[] args){
+                      Course cs=new Course();
+                      Student st=new Student();
+                      Teacher th=new Teacher();
+          }
+}
