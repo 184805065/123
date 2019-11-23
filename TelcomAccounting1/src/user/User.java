e898ca6ee0bda3666aec8205b856b67876495e06
wangji1314 <1346656676@qq.com>
2019-11-23 15:37:50 +0800

    提交

---
+++ TelcomAccounting1/src/user/User.java
@@ -0,0 +1,8 @@
+package user;
+
+public interface User {
+	void generateCommunicateRecord();
+	public String getCallToPhoneNumber();
+	public String accountFee(long timeStart, long timeEnd);
+	void printDetails();
+}
