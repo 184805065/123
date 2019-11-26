bb76f5db3d0f726201c81d667a2b415b872faf88
184805065 <55075106+184805065@users.noreply.github.com>
2019-11-26 23:11:28 +0800

    Add files via upload

---
+++ TelcomAccounting3/src/TelcomAccountSystem.java
@@ -0,0 +1,11 @@
+
+public class TelcomAccountSystem {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		TelcomUser telcomUser = new TelcomUser("13800138000");
+		telcomUser.generateCommunicateRecord();
+		telcomUser.printDetails();
+	}
+
+}
